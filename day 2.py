734cc13e510031b8ca46b99d5c665916480e88a7
Sara <361205433@qu.edu.sa>
2019-08-21 12:54:47 +0300

    comments lesson

---
+++ day 2.py
@@ -0,0 +1,11 @@
+# single line comment
+"""
+    docstring
+        /
+    multiline comment
+"""
+#print("100DaysOfCode")
+event="100DaysOfCode"
+print("one way: ",event)
+days=100
+print("another way: ",str(days)+"DaysOfCode")
