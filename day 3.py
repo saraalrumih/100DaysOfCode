50926eb7148419816588483d2fdcea4dcce76c26
Sara <361205433@qu.edu.sa>
2019-08-22 14:27:53 +0300

    variables lesson

---
+++ day 3.py
@@ -0,0 +1,10 @@
+#variables
+
+# adding two numbers and print the result
+n1, n2=5, 7
+print(n1," + ",n2," = ",(n1+n2))
+
+# print two texts
+welcome="Welcome to 100DaysOfCode"
+language="Python"
+print(welcome+" in "+language+"!")
