1a8e06e0f83bee9f45a95bd46eb751c3691a47ba
Sara <361205433@qu.edu.sa>
2019-09-18 16:31:32 +0300

    30th day

---
+++ day 030.py
@@ -0,0 +1,14 @@
+# for loop 2
+# print odd numbers between 1 to 10
+for i in range(1,10,2):
+    print(i)
+else:
+    print("These are the odd numbers between 1 to 10.\n\n")
+
+# nested for loops
+mothers =("Sara","Jana","Renad")
+daughters =("Mai","Fai","Nora")
+for mom in mothers:
+    for girl in daughters:
+        print(mom," is ",girl,"'s mother.")
+
