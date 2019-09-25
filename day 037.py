7a0ccd9f3cbae9f70322b1ba00f7eecf10fccf99
Sara <361205433@qu.edu.sa>
2019-09-25 15:47:38 +0300

    37th day

---
+++ day 037.py
@@ -0,0 +1,6 @@
+# arrays
+cart =["Apple", "Bread", "Toothpaste"]
+print("The cart contains: ",cart)
+print("the second element in the cart is: ", cart[1])
+cart[2]="Peans"
+print("The cart after changing the last element: ",cart)
