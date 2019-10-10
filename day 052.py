5fbbfcf641982ef80e4c08d62d46aa3d9231cf6d
Sara <361205433@qu.edu.sa>
2019-10-10 14:56:04 +0300

    52th day

---
+++ day 052.py
@@ -0,0 +1,5 @@
+# datetime
+import datetime as t
+print("current date and time is: ",t.datetime.now())
+print("current year is: ",t.datetime.now().year)
+print("today is: ",t.datetime.now().strftime("%A"))
